119c920096bfe0005ab1703f0672c4e73e610574
Niraj Kumar <nirajkvinit@gmail.com>
2017-07-18 20:17:26 +0530

    ch9 completed

--- ShreyaGuessingGame/app/src/main/java/com/example/niraj/shreyaguessinggame/MainActivity.java
+++ ShreyaGuessingGame/app/src/main/java/com/example/niraj/shreyaguessinggame/MainActivity.java
@@ -8,15 +8,74 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import java.lang.String;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText txtGuess;
+    private Button btnGuess;
+    private TextView lblOutput;
+    private int theNumber;
+
+    public void checkGuess() {
+        // Get the number the user entered
+        String theirNumber = txtGuess.getText().toString();
+        String message = "";
+        try {
+
+            int guess = Integer.parseInt(theirNumber);
+
+            if (guess > theNumber) { // too high
+                message = guess + " was too high. Guess again.";
+            }
+            else if (guess < theNumber) { // too low
+                message = guess + " was too low. Guess again.";
+            }
+            else {  // correct
+                message = guess + " was the right number. You win! Play again!";
+                newGame();
+            }
+
+            lblOutput.setText(message);
+        }
+        catch(Exception ex) {
+            message = " Please enter a whole number above.";
+            lblOutput.setText(message);
+        }
+        finally {   // highlight the txtGuess text field
+
+        }
+    }
+
+    private void newGame() {
+        theNumber = (int) (Math.random() * 100 + 1);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        txtGuess = (EditText) findViewById(R.id.txtGuess);
+        btnGuess = (Button) findViewById(R.id.btnGuess);
+        lblOutput = (TextView) findViewById(R.id.lblOutput);
+
+        newGame();
+
+        // setup the event listener for our guess button.
+        btnGuess.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkGuess();
+            }
+        });
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Shreya's Guessing Game");
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
